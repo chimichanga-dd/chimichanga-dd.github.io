8d54ea532f03647d2479febb403eaf87c6df003b
David Dixon <chimichanga@g.ucla.edu>
2020-03-06 14:11:14 -0800

    change welcome component to functional component

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,58 +1,13 @@
 
 import React from "react"
-import Typed from 'react-typed';
 
-class Welcome extends React.Component {
+const Welcome = () => {
 
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            showSecond: false
-        }
-
-        this.updateState = this.updateState.bind(this)
-    }
-
-    updateState(bool) {
-        this.setState({ showSecond: bool })
-    }
-
-    showSecond() {
-        if (this.state.showSecond) {
-            return <Typed
-                strings={['I am a Software Engineer']}
-                typeSpeed={80}
-                backDelay={1000}
-                backSpeed={40}
-                loop
-            />
-        }
-        return null
-    }
-
-    render() {
-
-        return (
-            <div className="welcome">
-                >  David Dixon
-                <div>
-                    <Typed
-                        strings={['My name is David Dixon']}
-                        typeSpeed={80}
-                        onComplete={(self) => {
-                            self.cursor.remove();
-                            this.updateState(true)
-                        }}
-                    />
-                </div>
-                <div>
-                    {this.showSecond()}
-                </div>
+    return (
+        <div className="welcome">
+            David Dixon
             </div>
-        )
-    }
-
+    )
 }
 
 export default Welcome
