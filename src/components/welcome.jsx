5fdac812638b08e66fc48e9d01bd2e8b2175b81a
David Dixon <chimichanga@g.ucla.edu>
2020-03-06 15:41:33 -0800

    add styling to welcome

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -5,7 +5,7 @@ const Welcome = () => {
 
     return (
         <div className="welcome">
-            <div className="name">> David Dixon</div>
+            <div className="name">David Dixon</div>
             <div className="mini-about">Former GIS practitioner turned 
                                         Software engineer who is creating projects
                                         with Ruby, Rails, React and much more.
