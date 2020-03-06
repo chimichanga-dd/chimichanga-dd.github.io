1d838b46371f1acbc2327e557889923c2b06f7c2
David Dixon <chimichanga@g.ucla.edu>
2020-03-05 21:46:46 -0800

    change console divs to li and add arrow spans

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -9,33 +9,59 @@ const About = () => {
                 <div className="yellow-button fake-icon"></div>
                 <div className="green-button fake-icon"></div>
             </div>
-            <div className="console">
-                <div className="contact-info input">
-                    David.contactInfo
-                </div>
-                <div className="contact-info output">
-                    ["<a href="mailto:chimichanga@g.ucla.edu" target="_blank" alt="email address">chimichanga@g.ucla.edu</a>",
-                    "<a href="https://www.linkedin.com/in/david-d-b36589190/" target="_blank" alt="linkedin link">LinkedIn</a>"],
-                    "<a href="https://github.com/chimichanga-dd" target="_blank" alt="github link">GitHub</a>"]
-                </div>
-                <div className="resume input">> David.resume</div>
-                <div className="resume output">
-                    ["<a href="https://chimichanga-dd.github.io/resume/" target="_blank" alt="resume link">Resume</a>"]
-                </div>
-                <div className="education input">> David.education</div>
-                <div className="education output">
-                    [" B.A. Geography - University of California Los Angeles", "Self Study - App Academy Full Stack Bootcamp"]
-                </div>
-                <div className="tech input">> David.techStack</div>
-                <div className="tech output">
-                    ["Ruby", "HTML", "Javascript", "CSS", "Rails", "React", "Redux", "PostgreSQL", "Python", "GIS"]
-                </div>
-                <div className="interests input">> David.interests</div>
-                <div className="interests output">
-                    ["Skateboarding", "Rock Climbing", "League of Legends", "Apparel"]
-                </div>
-                <div className="blank input"> > <div className="blinker"></div></div>
-            </div>
+            <ul className="console-bottom">
+
+                <li className="contact-info">
+                    <div className="contact-info input">
+                        <span className="arrow">></span>David.contactInfo
+                    </div>
+                    <div className="contact-info output">
+                            ["<a href="mailto:chimichanga@g.ucla.edu" target="_blank" alt="email address">chimichanga@g.ucla.edu</a>",
+                        "<a href="https://www.linkedin.com/in/david-d-b36589190/" target="_blank" alt="linkedin link">LinkedIn</a>"],
+                        "<a href="https://github.com/chimichanga-dd" target="_blank" alt="github link">GitHub</a>"]
+                    </div>
+                </li>
+
+                <li className="resume" key={"resume"}>
+                    <div className="resume input">
+                        <span className="arrow">></span>David.resume
+                    </div>
+                        <div className="resume output">
+                            ["<a href="https://chimichanga-dd.github.io/resume/" target="_blank" alt="resume link">Resume</a>"]
+                    </div>
+                </li>
+
+                <li className="education" key={"education"}>
+                    <div className="education input">
+                        <span className="arrow">></span>David.education
+                    </div>
+                        <div className="education output">
+                            [" B.A. Geography - University of California Los Angeles", "Self Study - App Academy Full Stack Bootcamp"]
+                    </div>
+                </li>
+
+                <li className="tech" key={"tech"}>
+                    <div className="tech input">
+                        <span className="arrow">></span>David.techStack
+                    </div>
+                        <div className="tech output">
+                            ["Ruby", "HTML", "Javascript", "CSS", "Rails", "React", "Redux", "PostgreSQL", "Python", "GIS"]
+                    </div>
+                </li>
+
+                <li className="interests" key={"interests"}>
+                    <div className="interests input">
+                        <span className="arrow">></span>David.interests
+                    </div>
+                        <div className="interests output">
+                            ["Skateboarding", "Rock Climbing", "League of Legends", "Apparel"]
+                    </div>
+                </li>
+                
+                <li className="blank input" key={"blank"}>
+                    <span className="arrow">></span><div className="blinker"></div>
+                </li>
+            </ul>
         </div>
     )
 
