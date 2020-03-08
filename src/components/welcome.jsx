9844ecba1bf3899cc0951419e12a91aecc07cabb
David Dixon <chimichanga@g.ucla.edu>
2020-03-07 22:09:05 -0800

    change welcome component to class

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,17 +1,80 @@
 
 import React from "react"
+import Typed from 'react-typed';
 
-const Welcome = () => {
+const lastName = () => (
+    <Typed
+        strings={["Dixon"]}
+        typeSpeed={30}
+        backDelay={2000}
+        loop
+    />
+)
 
-    return (
-        <div className="welcome">
-            <div className="name">David Dixon</div>
-            <div className="mini-about">Former GIS practitioner turned 
-                                        Software engineer who is creating projects
-                                        with Ruby, Rails, React and much more.
-             </div>
-        </div>
-    )
+
+
+class Welcome extends React.Component{
+
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            showLastName: false,
+            firstLoop: true
+        }
+        this.updateState = this.updateState.bind(this)
+    }
+
+    updateState(attr, bool) {
+        this.setState({ [attr]: bool })
+    }
+
+    showLastName() {
+
+        let lastName = this.state.firstLoop ? "Dixon" : "^1000Dixon"
+
+        if (this.state.showLastName) {
+            return <Typed
+                strings={[lastName]}
+                typeSpeed={80}
+                backDelay={3000}
+                backSpeed={40}
+                onComplete={() => this.updateState("firstLoop", false)}
+                loop
+            />
+        }
+        return null
+    }
+
+    render() {
+
+        let firstName = <Typed
+            strings={["> David "]}
+            typeSpeed={80}
+            onComplete={(self) => {
+                self.cursor.remove(); this.updateState("showLastName", true)}}
+        />
+
+        return (
+            <div className="welcome">
+                <div className="name">
+                    {firstName} {this.showLastName()}
+                </div>
+                <div className="mini-about">Former GIS practitioner turned
+                                            Software engineer who is creating projects
+                                            with Ruby, Rails, React and much more.
+                </div>
+            </div>
+        )
+
+
+
+}
+
+
+
+
+
 }
 
 export default Welcome
