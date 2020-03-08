f76ef23dc0c31b700b537ac4b710f36e5754979b
David Dixon <chimichanga@g.ucla.edu>
2020-03-08 13:45:23 -0700

    change welcome back to functional component

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -1,64 +1,12 @@
 
 import React from "react"
-import Typed from 'react-typed';
 
-const lastName = () => (
-    <Typed
-        strings={["Dixon"]}
-        typeSpeed={30}
-        backDelay={2000}
-        loop
-    />
-)
-
-
-
-class Welcome extends React.Component{
-
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            showLastName: false,
-            firstLoop: true
-        }
-        this.updateState = this.updateState.bind(this)
-    }
-
-    updateState(attr, bool) {
-        this.setState({ [attr]: bool })
-    }
-
-    showLastName() {
-
-        let lastName = this.state.firstLoop ? "Dixon" : "^1000Dixon"
-
-        if (this.state.showLastName) {
-            return <Typed
-                strings={[lastName]}
-                typeSpeed={80}
-                backDelay={3000}
-                backSpeed={40}
-                onComplete={() => this.updateState("firstLoop", false)}
-                loop
-            />
-        }
-        return null
-    }
-
-    render() {
-
-        let firstName = <Typed
-            strings={["> David "]}
-            typeSpeed={80}
-            onComplete={(self) => {
-                self.cursor.remove(); this.updateState("showLastName", true)}}
-        />
+const Welcome = () => {
 
         return (
             <div className="welcome">
                 <div className="name">
-                    {firstName} {this.showLastName()}
+                    <span className="blinker">></span> David Dixon <span className="blinker"> {`<`} </span>
                 </div>
                 <div className="mini-about">Former GIS practitioner turned
                                             Software engineer who is creating projects
@@ -66,15 +14,6 @@ class Welcome extends React.Component{
                 </div>
             </div>
         )
-
-
-
-}
-
-
-
-
-
 }
 
 export default Welcome
