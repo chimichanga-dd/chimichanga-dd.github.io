e70e552de3c6548159bf05306e6217a470015428
David Dixon <chimichanga@g.ucla.edu>
2020-03-08 13:09:33 -0700

    add classes to navbar links

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,10 +4,16 @@ import React from "react"
 const Nav = () => {
     return(
         <nav className="nav-bar">
-            <ul className="nav-bar-links">
-                <li key={"About"}><a href="#About">About</a></li>
-                <li key={"Projects"}><a href="#Projects">Projects</a></li>
-                <li key={"Contact"}><a href="#Contact">Contact</a></li>
+            <ul className="nav-bar-link-container">
+                <li key={"About"}>
+                    <a href="#About" className="nav-bar-link">About</a>
+                    </li>
+                <li key={"Projects"}>
+                    <a href="#Projects" className="nav-bar-link">Projects</a>
+                </li>
+                <li key={"Contact"}>
+                    <a href="#Contact" className="nav-bar-link">Contact</a>
+                </li>
             </ul>
             
         </nav>
