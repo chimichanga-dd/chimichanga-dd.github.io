ffb4a62f88ce250589e17288466d0f20f8ceb50d
David Dixon <chimichanga@g.ucla.edu>
2020-03-08 20:04:35 -0700

    add jquery for smooth scrolling

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React from "react";
 import Typed from 'react-typed';
 
 class About extends React.Component {
--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -1,5 +1,12 @@
 
 import React from "react"
+import $ from "jquery"
+
+const jqueryScroll = (eleID) => {
+    $('html, body').animate({
+        scrollTop: $(`#${eleID}`).offset().top
+    },800)
+}
 
 
 const Nav = () => {
@@ -9,21 +16,21 @@ const Nav = () => {
             <ul className="nav-bar-link-container">
                 <li key={"About"}>
                     <button
-                     onClick={(e) => {e.preventDefault(); }}
+                        onClick={(e) => { e.preventDefault(); jqueryScroll("About")}}
                     >
                         About
                     </button>
                     </li>
                 <li key={"Projects"}>
                     <button
-                     onClick={(e) => {e.preventDefault(); }}
+                     onClick={(e) => {e.preventDefault(); jqueryScroll("Projects")}}
                     >
                         Projects
                     </button>
                 </li>
                 <li key={"Contact"}>
                     <button
-                        onClick={(e) => { e.preventDefault(); }}
+                        onClick={(e) => { e.preventDefault(); jqueryScroll("Contact")}}
                     >
                         Contact
                     </button>
