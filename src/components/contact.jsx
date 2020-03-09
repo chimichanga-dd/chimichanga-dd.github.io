cd385e98197e0c845b9cd7736f29423e23c32e77
David Dixon <chimichanga@g.ucla.edu>
2020-03-09 15:42:57 -0700

    add email to contact

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -13,11 +13,18 @@ const Contact = () => {
                 Contact
             </h2>
             <div className="line-break"></div>
-            <ul className="contact-list">
-                <li><a href="https://www.linkedin.com/in/david-d-b36589190/" target="_blank" alt="linkedin link"><img src={linkedIn} alt={"linkedIn logo"} className="logo"/></a></li>
-                <li><img src={hs} alt="headshot" className="contact-headshot" /></li>
-                <li><a href="https://github.com/chimichanga-dd" target="_blank" alt="github link"><img src={gitHub} alt={"gitHub logo"} className="logo"/></a></li>
-            </ul>
+            <div className="contact-list-container">
+                <ul className="contact-list-top">
+                    <li><a href="https://www.linkedin.com/in/david-d-b36589190/" target="_blank" alt="linkedin link"><img src={linkedIn} alt={"linkedIn logo"} className="logo" /></a></li>
+                    <li><img src={hs} alt="headshot" className="contact-headshot" /></li>
+                    <li><a href="https://github.com/chimichanga-dd" target="_blank" alt="github link"><img src={gitHub} alt={"gitHub logo"} className="logo" /></a></li>
+                </ul>
+                <ul className="contact-list-bottom">
+                    <li className="contact-email">
+                        <a href="mailto:chimichanga@g.ucla.edu" target="_blank" alt="email address" rel="noopener">chimichanga@g.ucla.edu</a>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 
