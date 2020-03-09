64a852baf6e3076b97fc3d402ca6c42cd48ee5e0
David Dixon <chimichanga@g.ucla.edu>
2020-03-09 11:40:01 -0700

    add hashrouter for routing and resume

--- src/components/portfolio.jsx
+++ src/components/portfolio.jsx
@@ -1,27 +1,38 @@
 
-import React from "react"
-import Nav from "./nav"
-import Welcome from "./welcome"
-import About from "./about"
-import Projects from "./projects"
-import Contact from "./contact"
+import { HashRouter, Switch, Route } from "react-router-dom";
+
+import React from "react";
+import Nav from "./nav";
+import Welcome from "./welcome";
+import About from "./about";
+import Projects from "./projects";
+import Contact from "./contact";
+import Resume from "./resume";
 
 
+class portfolio extends React.Component {
 
-class portfolio extends React.Component{
-
-    render(){
-        return(
-            <div className="portfolio">
-                <Nav />
-                <Welcome />
-                <About />
-                <Projects />
-                <Contact />
-            </div>
+    render() {
+        return (
+            <HashRouter>
+                <Switch>
+                    <Route path="/resume">
+                        <Resume />
+                    </Route>
+                    <Route path="/">
+                        <div className="portfolio">
+                            <Nav />
+                            <Welcome />
+                            <About />
+                            <Projects />
+                            <Contact />
+                        </div>
+                    </Route>
+                </Switch>
+            </HashRouter>
         )
     }
-    
+
 }
 
 export default portfolio
