ee2d4acc1f353c9762332447af7df927e870b65a
David Dixon <chimichanga@g.ucla.edu>
2020-03-08 18:11:57 -0700

    add links to images

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -4,18 +4,19 @@ import hs from "../images/hs2.jpg";
 import linkedIn from "../images/LinkedIn-Logos/LI-In-Bug.png";
 import gitHub from "../images/GitHubMark/GitHub-Mark-64px.png"
 
+
 const Contact = () => {
 
     return(
-        <div className="contact-container">
+        <div id="Contact" className="contact-container">
             <h2 className="section-header">
                 Contact
             </h2>
             <div className="line-break"></div>
             <ul className="contact-list">
-                <li><img src={linkedIn} alt={"linkedIn logo"} className="logo"/></li>
+                <li><a href="https://www.linkedin.com/in/david-d-b36589190/" target="_blank" alt="linkedin link"><img src={linkedIn} alt={"linkedIn logo"} className="logo"/></a></li>
                 <li><img src={hs} alt="headshot" className="contact-headshot" /></li>
-                <li><img src={gitHub} alt={"gitHub logo"} className="logo"/></li>
+                <li><a href="https://github.com/chimichanga-dd" target="_blank" alt="github link"><img src={gitHub} alt={"gitHub logo"} className="logo"/></a></li>
             </ul>
         </div>
     )
