d50758009c505998a9513d3acf2b2a099e9bfaac
David Dixon <chimichanga@g.ucla.edu>
2020-03-09 11:39:18 -0700

    add resume iframe

---
+++ src/components/resume.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+const Resume = () => {
+    return (
+        <iframe
+            src="https://docs.google.com/gview?url=https://github.com/chimichanga-dd/resume/raw/master/Resume.pdf&embedded=true"
+            style={{ width: 100 + "%", height: 100 + "%" }}
+            frameBorder="0">
+        </iframe>
+    )
+}
+
+export default Resume
