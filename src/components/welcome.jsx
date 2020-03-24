a9bb68b28627ba049ac4b765a7d29039b976a184
David Dixon <chimichanga@g.ucla.edu>
2020-03-24 15:02:08 -0700

    change engineer to developer

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -14,7 +14,7 @@ const Welcome = () => {
                     </span>
                 </div>
                 <div id="About" className="mini-about">Former information systems analyst turned
-                                            software engineer creating projects
+                                            software developer creating projects
                                             with Ruby, Rails, React and much more.
                 </div>
             </div>
