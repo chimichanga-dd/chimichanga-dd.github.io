f4b4164d52ad4d2188f1f8ba4b30f564a7a4da88
David Dixon <chimichanga@g.ucla.edu>
2020-04-29 10:56:05 -0700

    remove secure from iframe path url

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -3,7 +3,7 @@ import React from "react"
 const Resume = () => {
     return (
         <iframe
-            src="https://docs.google.com/gview?url=https://github.com/chimichanga-dd/resume/raw/master/David_Dixon_Resume.pdf&embedded=true"
+            src="http://docs.google.com/gview?url=http://github.com/chimichanga-dd/resume/raw/master/David_Dixon_Resume.pdf&embedded=true"
             style={{ width: 100 + "%", height: 100 + "%" }}
             frameBorder="0">
         </iframe>
