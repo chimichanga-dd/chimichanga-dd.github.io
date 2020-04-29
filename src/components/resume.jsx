25464fb0518b1d120beec3a583430afcd84f94de
David Dixon <chimichanga@g.ucla.edu>
2020-04-29 13:07:28 -0700

    change resume function to refresh onload

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,9 +1,17 @@
 import React from "react"
 
 const Resume = () => {
+
+  const iframeRef = React.useRef(null)
+
+  React.useEffect( () => {
+    iframeRef.current.src = "http://docs.google.com/gview?url=http://github.com/chimichanga-dd/resume/raw/master/David_Dixon_Resume.pdf&embedded=true"
+  })
+
     return (
         <iframe
-            src="http://docs.google.com/gview?url=http://github.com/chimichanga-dd/resume/raw/master/David_Dixon_Resume.pdf&embedded=true"
+        ref={iframeRef}
+        src="http://docs.google.com/gview?url=http://github.com/chimichanga-dd/resume/raw/master/David_Dixon_Resume.pdf&embedded=true"
             style={{ width: 100 + "%", height: 100 + "%" }}
             frameBorder="0">
         </iframe>
