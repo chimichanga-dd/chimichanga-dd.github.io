c61a558c78a58ea0689ca3d6488886943afa55f9
David Dixon <chimichanga@g.ucla.edu>
2023-09-14 18:52:04 +0700

    add resume as file and remove url-loader

--- src/components/resume.jsx
+++ src/components/resume.jsx
@@ -1,21 +1,14 @@
-import React from "react"
+import React from "react";
+import resume from "../images/resume.pdf";
 
 const Resume = () => {
+  return (
+    <object
+      data={resume}
+      width="100%"
+      height="100%"
+    />
+  );
+};
 
-  const iframeRef = React.useRef(null)
-
-  React.useEffect( () => {
-    iframeRef.current.src = "https://docs.google.com/gview?url=http://github.com/chimichanga-dd/resume/raw/master/David_Dixon_Resume.pdf&embedded=true"
-  })
-
-    return (
-        <iframe
-        ref={iframeRef}
-        src="https://docs.google.com/gview?url=http://github.com/chimichanga-dd/resume/raw/master/David_Dixon_Resume.pdf&embedded=true"
-            style={{ width: 100 + "%", height: 100 + "%" }}
-            frameBorder="0">
-        </iframe>
-    )
-}
-
-export default Resume
+export default Resume;
--- webpack.config.js
+++ webpack.config.js
@@ -1,42 +1,34 @@
-
 module.exports = {
-    entry: [
-        './src/index.js'
-    ],
-    module: {
-        rules: [
-            {
-                test: [/\.jsx?$/, /\.js?$/],
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: [
-                            "@babel/env",
-                            "@babel/react"
-                        ],
-                    }
-                }
-            }, 
-            {
-                test: /\.(jpg|png)$/,
-                use: {
-                    loader: 'url-loader',
-                },
-            },
-        ]
-    },
-    resolve: {
-        extensions: [".js", ".jsx", "*"]
-    },
-    devServer:{
-        contentBase: "./",
-        port: 3000
-    },
-    output: {
-        path: __dirname + '/dist',
-        publicPath: '/dist',
-        filename: 'main.js'
-    },
-    devtool: "source-map"
-}
+  entry: ["./src/index.js"],
+  module: {
+    rules: [
+      {
+        test: [/\.jsx?$/, /\.js?$/],
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/env", "@babel/react"]
+          }
+        }
+      },
+      {
+        test: /\.(jpg|png|pdf)$/,
+        type: "asset/resource"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  },
+  devServer: {
+    contentBase: "./",
+    port: 3000
+  },
+  output: {
+    path: __dirname + "/dist",
+    publicPath: "/dist/",
+    filename: "main.js"
+  },
+  devtool: "source-map"
+};
